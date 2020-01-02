409d880464da295f87a3e1658c3cb396e5307adb
wesley perez <wesley.m.perez@gmail.com>
2020-01-02 18:51:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  # code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
+roll_call_dwarves(dwarves)
+planeteer_calls = %w[earth wind fire water heart]
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)
+  # code an argument here
   # Your code here
+   planeteer_calls.map { |call| call.capitalize + '!' }
 end
+summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planterr_calls)
+  # code an argument here
   # Your code here
+  planeteer_calls.any? { |call| call.length > 4 }
 end
+long_planeteer_calls(planeteer_calls)
+potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
-def find_the_cheese# code an argument here
+def find_the_cheese(potentially_cheesy_items)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheese_types.include?(maybe_cheese)
+  end
 end
+find_the_cheese(potentially_cheesy_items)
